6f19922bf211e7de756432d07cbb6df08344aecc
Samuel Šulovský <samuel.sulovsky@gmail.com>
2023-11-29 19:02:40 +0100

    Repo setup

---
+++ lib/aoc2023.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2023 do
+  @moduledoc """
+  Documentation for `Aoc2023`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2023.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
